9ac0fdeed24c287a5b96e63d1a0c80e4ca6bf4e5
vinoth <vinoth@payrexx.com>
2025-12-30 13:25:58 +0530

    bugfix/PP-17938: upgraded payrexx sdk

--- payrexx/vendor/composer/installed.php
+++ payrexx/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'payrexx/payrexxpaymentgateway',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '68cb85822bedb5f705dfe854b9cc2e2694be83b7',
+        'reference' => 'c939835e1ff0ed0bc02cf8caab3fca3af0e4c464',
         'type' => 'prestashop-module',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'payrexx/payrexx' => array(
-            'pretty_version' => 'v2.0.7',
-            'version' => '2.0.7.0',
-            'reference' => '2488323455e0508e99ebe58204d8112a8487efb8',
+            'pretty_version' => 'v2.0.8',
+            'version' => '2.0.8.0',
+            'reference' => '95473689ba082d327effbbc43fc2302d36c6c8bd',
             'type' => 'library',
             'install_path' => __DIR__ . '/../payrexx/payrexx',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'payrexx/payrexxpaymentgateway' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '68cb85822bedb5f705dfe854b9cc2e2694be83b7',
+            'reference' => 'c939835e1ff0ed0bc02cf8caab3fca3af0e4c464',
             'type' => 'prestashop-module',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
